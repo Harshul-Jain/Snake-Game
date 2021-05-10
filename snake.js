45b091f64f04e5794f14f05355de16db1c1dfceb
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-05-10 11:55:41 +0530

    Created Board And Snake

---
+++ snake.js
@@ -0,0 +1,40 @@
+function init(){
+ 	var canvas=document.getElementById('mycanvas');
+ 	W=H=canvas.width=canvas.height=1000;
+ 	pen=canvas.getContext('2d')
+ 	cs=66;
+ 	snake={
+ 		init_len:5,
+ 		color:"blue",
+ 		cells:[],
+ 		direction:"right",
+ 
+ 		createSnake:function(){
+ 			for(var i=this.init_len;i>0;i--)
+ 			{
+ 				this.cells.push({x:i,y:0});
+
+ 			}
+ 		},
+ 		drawSnake:function(){
+ 			pen.fillStyle="Blue";
+ 			for(var i=0;i<this.cells.length;i++){
+ 				pen.fillRect(this.cells[i].x*cs,this.cells[i].y,cs-2,cs-2);
+ 			}
+ 		}
+	};
+	snake.createSnake();
+}
+function draw(){
+	snake.drawSnake();
+
+}
+function update(){
+
+}
+function gameloop(){
+	draw();
+	update();
+}
+init();
+var f=setInterval(gameloop,100);
