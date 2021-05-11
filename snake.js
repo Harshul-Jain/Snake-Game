b503b366e92e177601965afe756e42d7606da04e
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-05-11 11:12:12 +0530

    Keyboard Input

--- snake.js
+++ snake.js
@@ -19,20 +19,50 @@ function init(){
  		drawSnake:function(){
  			pen.fillStyle=this.color;
  			for(var i=0;i<this.cells.length;i++){
- 				pen.fillRect(this.cells[i].x*cs,this.cells[i].y,cs-2,cs-2);
+ 				pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-2,cs-2);
  			}
  		},
  		updateSnake:function(){
  			this.cells.pop();
  			var headX=this.cells[0].x;
  			var headY=this.cells[0].y;
- 			var X=headX+1;
- 			var Y=headY;
- 			this.cells.unshift({x:X,y:Y});
+ 			var nextX,nextY;
+ 			if(this.direction=="right"){
+ 				nextX=headX+1;
+ 				nextY=headY;
+ 			}
+ 			else if(this.direction=="left"){
+ 				nextX=headX-1;
+ 				nextY=headY;
+ 			}
+ 			else if(this.direction=="down"){
+ 				nextX=headX;
+ 				nextY=headY+1;
+ 			}
+ 			else{
+ 				nextX=headX;
+ 				nextY=headY-1;
+ 			}
+ 			this.cells.unshift({x:nextX,y:nextY});
 
  		}
 	};
 	snake.createSnake();
+	function keyPressed(e){
+		if(e.key=="ArrowRight"){
+			snake.direction="right";
+		}
+		else if(e.key=="ArrowLeft"){
+			snake.direction="left";
+		}
+		else if(e.key=="ArrowDown"){
+			snake.direction="down";
+		}
+		else{
+			snake.direction="up";
+		}
+	}
+	document.addEventListener('keydown',keyPressed);
 }
 function draw(){
 	pen.clearRect(0,0,W,H);
